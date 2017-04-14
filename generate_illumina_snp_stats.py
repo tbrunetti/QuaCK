ff5285357e3233836148098606c0bac25bec23f5
tbrunetti <tonya.brunetti@colorado.edu>
2017-04-14 14:18:49 -0600

    completely restructured for vastly increased speed, added snp removal reasons

--- generate_illumina_snp_stats.py
+++ generate_illumina_snp_stats.py
@@ -9,6 +9,7 @@ import seaborn as sns
 
 def illumina_snp_overview(inputFile, pdf, clusterSep, aatmean, aatdev, bbtmean, bbtdev, aarmean, abrmean, bbrmean, callrate, outDir):
 	print "Running Illumina SNP QC"
+
 	snp_qc_table = pandas.read_table(inputFile)
 	snp_qc_table[['Chr']] = snp_qc_table[['Chr']].astype(str)
 	chr_strings = [str(chrm) for chrm in range(1, 23)]
@@ -69,45 +70,107 @@ def illumina_snp_overview(inputFile, pdf, clusterSep, aatmean, aatdev, bbtmean, 
 		all_stats[header_stats] = [stats.median(list(snp_qc_table[header_stats])), stats.mean(list(snp_qc_table[header_stats])), stats.stdev(list(snp_qc_table[header_stats])), 
 					min(list(snp_qc_table[header_stats])), max(list(snp_qc_table[header_stats]))]
 
-	
-	# snp failure stats call rate, autosomes 
-	stats_auto_snp_callrate = [stats.median(list(autosomes_only['Call Freq'])), stats.mean(list(autosomes_only['Call Freq'])), stats.stdev(list(autosomes_only['Call Freq'])), 
-					min(list(autosomes_only['Call Freq'])), max(list(autosomes_only['Call Freq']))]
+	print "		Extracting failed SNPs"
 
-	print "		Extracting failed SNPs"				
-	# cluster separation calculations
-	snps_fail_clus_sep = list(snp_qc_table[snp_qc_table[clus_sep.group(0)] <= float(clusterSep)]['Name'])
+
+	dicts_to_merge = []
+	############ cluster separation calculations ###############
+	
+	# make dataframe of failed SNPs
+	snps_fail_clus_sep_dataframe = snp_qc_table[snp_qc_table[clus_sep.group(0)] <= float(clusterSep)]
+	# append string to front of value of failed threshold to prepare for dictionary format
+	snps_fail_clus_sep_dataframe[clus_sep.group(0)] = 'failed_cluster_separation: '+snps_fail_clus_sep_dataframe[clus_sep.group(0)].astype(str)
+	# make dictionary of snp (key) and reasons with value of why snp failed (value)
+	snps_fail_clust_dict = snps_fail_clus_sep_dataframe.set_index('Name')[clus_sep.group(0)].to_dict()	
+	# make dictionary value an iterable to easy merging at end
+	snps_fail_clust_dict_appendable = {key:[value] for key, value in snps_fail_clust_dict.iteritems()}
+	# list of snps that fail cluster separation
+	snps_fail_clus_sep = [key for key in snps_fail_clust_dict]
 	total_snps_passing_clust = total_snps - len(snps_fail_clus_sep)
-
-	# AA_T mean score calculations
-	snps_fail_AATmean = list(snp_qc_table[snp_qc_table[AATmean.group(0)] > float(aatmean)]['Name'])
-	total_snps_passing_AATmean = total_snps - len(snps_fail_AATmean)
-
-	# AA_T dev score calculations
-	snps_fail_AATdev = list(snp_qc_table[snp_qc_table[AATdev.group(0)] > float(aatdev)]['Name'])
-	total_snps_passing_AATdev = total_snps - len(snps_fail_AATdev)
-
-	# BB T mean score calculations
-	snps_fail_BBTmean = list(snp_qc_table[snp_qc_table[BBTmean.group(0)] < float(bbtmean)]['Name'])
-	total_snps_passing_BBTmean = total_snps - len(snps_fail_BBTmean)
-
-	# BB T dev score calculations
-	snps_fail_BBTdev = list(snp_qc_table[snp_qc_table[BBTdev.group(0)] > float(bbtdev)]['Name'])
-	total_snps_passing_BBTdev = total_snps - len(snps_fail_BBTdev)
-
-	# AA R mean score calculations
-	snps_fail_AARmean = list(snp_qc_table[snp_qc_table[AARmean.group(0)] <= float(aarmean)]['Name'])
-	total_snps_passing_AARmean = total_snps - len(snps_fail_AARmean)
-
-	# AB R mean score calculations
-	snps_fail_ABRmean = list(snp_qc_table[snp_qc_table[ABRmean.group(0)] <= float(abrmean)]['Name'])
-	total_snps_passing_ABRmean = total_snps - len(snps_fail_ABRmean)
-
-	# BB R mean score calculations
-	snps_fail_BBRmean = list(snp_qc_table[snp_qc_table[BBRmean.group(0)] <= float(bbrmean)]['Name'])
-	total_snps_passing_BBRmean = total_snps - len(snps_fail_BBRmean)
+	# append dictionary to list of dictionaries that need merging
+	dicts_to_merge.append(snps_fail_clust_dict_appendable)
 
 	
+	############### AA_T mean score calculations ##################
+	snps_fail_AATmean_dataframe = snp_qc_table[snp_qc_table[AATmean.group(0)] > float(aatmean)]
+	snps_fail_AATmean_dataframe[AATmean.group(0)] = 'failed AAT mean threshold: '+ snps_fail_AATmean_dataframe[AATmean.group(0)].astype(str)
+	snps_fail_AATmean_dict = snps_fail_AATmean_dataframe.set_index('Name')[AATmean.group(0)].to_dict()	
+	snps_fail_AATmean_dict_appendable = {key:[value] for key, value in snps_fail_AATmean_dict.iteritems()}
+	snps_fail_AATmean = [key for key in snps_fail_AATmean_dict]
+	total_snps_passing_AATmean = total_snps - len(snps_fail_AATmean)
+	dicts_to_merge.append(snps_fail_AATmean_dict_appendable)
+
+
+	################ AA_T dev score calculations ###############
+	snps_fail_AATdev_dataframe = snp_qc_table[snp_qc_table[AATdev.group(0)] > float(aatdev)]
+	snps_fail_AATdev_dataframe[AATdev.group(0)] = 'failed AAT dev threshold: ' + snps_fail_AATdev_dataframe[AATdev.group(0)].astype(str)
+	snps_fail_AATdev_dict = snps_fail_AATdev_dataframe.set_index('Name')[AATdev.group(0)].to_dict()
+	snps_fail_AATdev_dict_appendable = {key:[value] for key, value in snps_fail_AATdev_dict.iteritems()}
+	snps_fail_AATdev = [key for key in snps_fail_AATdev_dict]
+	total_snps_passing_AATdev = total_snps - len(snps_fail_AATdev)
+	dicts_to_merge.append(snps_fail_AATdev_dict_appendable)
+
+	
+	################ BB T mean score calculations ##################
+	snps_fail_BBTmean_dataframe = snp_qc_table[snp_qc_table[BBTmean.group(0)] < float(bbtmean)]
+	snps_fail_BBTmean_dataframe[BBTmean.group(0)] = 'failed BBT mean threshold: ' + snps_fail_BBTmean_dataframe[BBTmean.group(0)].astype(str)
+	snps_fail_BBTmean_dict = snps_fail_BBTmean_dataframe.set_index('Name')[BBTmean.group(0)].to_dict()
+	snps_fail_BBTmean_dict_appendable = {key:[value] for key, value in snps_fail_BBTmean_dict.iteritems()}
+	snps_fail_BBTmean = [key for key in snps_fail_BBTmean_dict]
+	total_snps_passing_BBTmean = total_snps - len(snps_fail_BBTmean)
+	dicts_to_merge.append(snps_fail_BBTmean_dict_appendable)
+
+
+	################# BB T dev score calculations ######################
+	snps_fail_BBTdev_dataframe = snp_qc_table[snp_qc_table[BBTdev.group(0)] > float(bbtdev)]
+	snps_fail_BBTdev_dataframe[BBTdev.group(0)] = 'failed BBT dev treshold: ' + snps_fail_BBTdev_dataframe[BBTdev.group(0)].astype(str)
+	snps_fail_BBTdev_dict = snps_fail_BBTdev_dataframe.set_index('Name')[BBTdev.group(0)].to_dict()
+	snps_fail_BBTdev_dict_appendable = {key:[value] for key, value in snps_fail_BBTdev_dict.iteritems()}
+	snps_fail_BBTdev = [key for key in snps_fail_BBTdev_dict]
+	total_snps_passing_BBTdev = total_snps - len(snps_fail_BBTdev)
+	dicts_to_merge.append(snps_fail_BBTdev_dict_appendable)
+
+
+	################# AA R mean score calculations ######################
+	snps_fail_AARmean_dataframe = snp_qc_table[snp_qc_table[AARmean.group(0)] <= float(aarmean)]
+	snps_fail_AARmean_dataframe[AARmean.group(0)] = 'failed AAR mean threshold: ' + snps_fail_AARmean_dataframe[AARmean.group(0)].astype(str)
+	snps_fail_AARmean_dict = snps_fail_AARmean_dataframe.set_index('Name')[AARmean.group(0)].to_dict()
+	snps_fail_AARmean_dict_appendable = {key:[value] for key, value in snps_fail_AARmean_dict.iteritems()}
+	snps_fail_AARmean = [key for key in snps_fail_AARmean_dict]
+	total_snps_passing_AARmean = total_snps - len(snps_fail_AARmean)
+	dicts_to_merge.append(snps_fail_AARmean_dict_appendable)
+
+
+	################ AB R mean score calculations #######################
+	snps_fail_ABRmean_dataframe = snp_qc_table[snp_qc_table[ABRmean.group(0)] <= float(abrmean)]
+	snps_fail_ABRmean_dataframe[ABRmean.group(0)] = 'failed ABR mean threshold: ' + snps_fail_ABRmean_dataframe[ABRmean.group(0)].astype(str)
+	snps_fail_ABRmean_dict = snps_fail_ABRmean_dataframe.set_index('Name')[ABRmean.group(0)].to_dict()
+	snps_fail_ABRmean_dict_appendable = {key:[value] for key, value in snps_fail_ABRmean_dict.iteritems()}
+	snps_fail_ABRmean = [key for key in snps_fail_ABRmean_dict]
+	total_snps_passing_ABRmean = total_snps - len(snps_fail_ABRmean)
+	dicts_to_merge.append(snps_fail_ABRmean_dict_appendable)
+
+	
+	################# BB R mean score calculations #######################3
+	snps_fail_BBRmean_dataframe = snp_qc_table[snp_qc_table[BBRmean.group(0)] <= float(bbrmean)]
+	snps_fail_BBRmean_dataframe[BBRmean.group(0)] = 'failed BBR mean threshold: ' + snps_fail_BBRmean_dataframe[BBRmean.group(0)].astype(str)
+	snps_fail_BBRmean_dict = snps_fail_BBRmean_dataframe.set_index('Name')[BBRmean.group(0)].to_dict()
+	snps_fail_BBRmean_dict_appendable = {key:[value] for key, value in snps_fail_BBRmean_dict.iteritems()}
+	snps_fail_BBRmean = [key for key in snps_fail_BBRmean_dict]
+	total_snps_passing_BBRmean = total_snps - len(snps_fail_BBRmean)
+	dicts_to_merge.append(snps_fail_BBRmean_dict_appendable)
+
+
+	# keep track of snps failing and why failing
+	reasons_snps_fail = {}
+	# merge all dictionaries without overwriting values
+	for dictionary in dicts_to_merge:
+		for key, value in dictionary.iteritems():
+			try:
+				reasons_snps_fail.setdefault(key, []).extend(value)
+			except TypeError: # in the event the dictionary is not iterable (list)
+				reasons_snps_fail[key].append(value)
+
 	# concatenate all failing SNPs
 	snps_to_remove = list(set(snps_fail_clus_sep + snps_fail_AATmean + snps_fail_AATdev + snps_fail_BBTmean + snps_fail_BBTdev + snps_fail_AARmean + snps_fail_ABRmean + snps_fail_BBRmean))
 
@@ -121,7 +184,6 @@ def illumina_snp_overview(inputFile, pdf, clusterSep, aatmean, aatdev, bbtmean, 
 
 	missing_remain_table = missing_chr.loc[~missing_chr['Name'].isin(snps_to_remove)]
 	missing_remain = len(missing_remain_table.index)
-
 
 
 
@@ -316,4 +378,4 @@ def illumina_snp_overview(inputFile, pdf, clusterSep, aatmean, aatdev, bbtmean, 
 
 
 	print "		...Finished writing Illumina SNP QC statistics..."	
-	return snps_to_remove
+	return snps_to_remove, reasons_snps_fail
